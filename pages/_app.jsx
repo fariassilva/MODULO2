10520e196f85c491b38ae56203d6d064515627a2
fariassilva <alessandrofarias405@gmail.com>
2022-12-15 07:50:04 -0300

    configuração do padrão

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,21 @@
+import Head from 'next/head'
+
 import '../src/Styles.css'
 function App({Component,pageProps}){
 
-  return <Component {...pageProps}/>
+  return(
+  <>
+
+
+  <Head>
+ <title>titulo padrao</title>
+  </Head>
+  <Component {...pageProps}/>
+
+  </>
+
+
+  )
+
 }
 export default App
