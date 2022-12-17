d50a194906f5d75369a7f0753c47806c2b0ed29c
fariassilva <alessandrofarias405@gmail.com>
2022-12-17 08:58:50 -0300

    outro exemplo

---
+++ pages/exemplo.jsx
@@ -0,0 +1,19 @@
+import Header from "../src/comonents/header/Header"
+import Title from "../src/comonents/title/Title"
+import Subtitle from "../src/comonents/subtitle/Subtitle"
+
+function ExemploPage(){
+
+  return(
+
+    <>
+     <Header>
+      <Title> cafe dev</Title>
+      <Subtitle text="o melhor café para o programador"/>
+     </Header>
+    </>
+
+  )
+}
+
+export default ExemploPage
---
+++ pages/outroexemplo.jsx
@@ -0,0 +1,21 @@
+import Header from "../src/comonents/header/Header"
+import Title from "../src/comonents/title/Title"
+import Subtitle from "../src/comonents/subtitle/Subtitle"
+
+function OutroExemploPage(){
+ 
+  return(
+    <>
+     <Header>
+      <Title>  outro cafe dev</Title>
+      <Subtitle text="o segundo melhor café para o programador"/>
+     </Header>
+    </>
+
+
+  )
+
+
+
+}
+export default OutroExemploPage
